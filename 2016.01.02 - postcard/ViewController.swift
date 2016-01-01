a11b97bc1bf43559b52b5ac84513da9e3bdf5558
Norman Kilpatrick <NormK@AW-A1>
2016-01-02 02:08:25 +1100

    Initial Commit

---
+++ 2016.01.02 - postcard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2016.01.02 - postcard
+//
+//  Created by Norman Kilpatrick on 2/01/2016.
+//  Copyright © 2016 Norman Kilpatrick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
